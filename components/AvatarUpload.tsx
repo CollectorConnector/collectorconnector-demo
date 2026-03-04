0a6374b2346d8c7b933bc15f2404d13818f47b37
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 20:24:50 +0000

    Update AvatarUpload.tsx

--- components/AvatarUpload.tsx
+++ components/AvatarUpload.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '@/lib/supabase';
 
 export default function AvatarUpload({
   userId,

cc45a577475ea823a4f78e37807a42824bb8a0a4
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 07:57:12 +0000

    Create globals.css

---
+++ app/app/globals.css
@@ -0,0 +1,9 @@
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background: black;
+  color: white;
+}
+
+a { color: #4ADE80; text-decoration: none; }
+input, textarea, select { color: black; }

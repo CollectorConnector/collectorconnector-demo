e42614ad4fc7eeb7daac5de5a6c8625105f8a477
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 07:35:25 +0000

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-body {
-  margin: 0;
-  font-family: Arial, sans-serif;
-  background: black;
-  color: white;
-}
-
-a { color: #4ADE80; text-decoration: none; }
-input, textarea, select { color: black; }

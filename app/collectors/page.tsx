0ee6b42766a384236da82835a68154c1a927b547
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 13:11:01 +0000

    Create page.tsx

---
+++ app/collectors/page.tsx
@@ -0,0 +1,9 @@
+
+export default function CollectorsPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Collectors</h1>
+      <p style={{ color: "#9CA3AF" }}>Directory coming soon.</p>
+    </div>
+  );
+}

02f4d44b462f6e7e96a55dd36445d84195e53acc
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 08:01:30 +0000

    Delete app directory

--- app/app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Welcome to CollectorConnector</h1>
-      <p>Create and showcase your collector identity.</p>
-      <a href="/create-profile">Create your profile →</a>
-    </div>
-  );
-}

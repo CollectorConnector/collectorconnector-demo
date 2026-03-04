1493aca17b04616e35751f15e8d284612b34484c
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-04 11:39:53 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body style={{ background: "#000", color: "#fff" }}>
-
+        
+        {/* NAVIGATION BAR */}
         <nav
           style={{
             width: "100%",
@@ -27,23 +28,27 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           }}
         >
           {/* LEFT SIDE - APP NAME */}
-          /
+          <a
+            href="/"
+            style={{
+              color: "#fff",
+              fontSize: "20px",
+              fontWeight: 700,
+              textDecoration: "none",
+            }}
+          >
             CollectorConnector
           </a>
 
           {/* RIGHT SIDE - NAV LINKS */}
           <div style={{ display: "flex", gap: "24px", alignItems: "center" }}>
-            /Home</a>
-            /collectorsCollectors</a>
-            /create-profileCreate Profile</a>
-            /auth/login
+            <a href="/" style={{ color: "#fff", textDecoration: "none" }}>Home</a>
+            <a href="/collectors" style={{ color: "#fff", textDecoration: "none" }}>Collectors</a>
+            <a href="/create-profile" style={{ color: "#fff", textDecoration: "none" }}>Create Profile</a>
+            <a href="/auth/login" style={{ color: "#4ADE80", textDecoration: "none", fontWeight: 600 }}>
               Login
             </a>
           </div>
         </nav>
 
         {children}
-      </body>
-    </html>
-  );
-}

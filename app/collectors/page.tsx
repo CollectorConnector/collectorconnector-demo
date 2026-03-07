3ba9726fdb6a61cc45d39c7ead7a4a27f22c8d34
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-07 20:59:16 +0000

    Delete app/collectors directory

--- app/collectors/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function CollectorsPage() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Collectors</h1>
-      <p style={{ color: "#9CA3AF" }}>Directory coming soon.</p>
-    </div>
-  );
-}

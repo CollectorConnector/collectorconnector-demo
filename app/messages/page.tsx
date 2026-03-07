6df8563f5cf4bb80f733d0bffe3466592ed18061
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-07 20:22:01 +0000

    Create page.tsx

---
+++ app/messages/page.tsx
@@ -0,0 +1,11 @@
+[
+  {
+    id: "123",
+    name: "Richard House",
+    avatar: "/default-avatar.png",
+    tier: "FOUNDER",
+    lastMessage: "It’s looking amazing already.",
+    time: "2h ago"
+  },
+  ...
+]

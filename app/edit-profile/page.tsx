3f5bbd99f64133f754b87c4d15789b1b821a12e8
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-08 07:07:43 +0000

    Update page.tsx

--- app/edit-profile/page.tsx
+++ app/edit-profile/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "@/utils/supabase/client";
 import TierBadge from "@/components/TierBadge";
 
 export default function EditProfilePage() {

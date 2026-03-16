3c9a67de090586fcac611d429f99c88ddf13c3b4
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-16 12:57:22 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white antialiased">
+      <body className="bg-black text-white antialiased w-full min-w-full">
         {children}
       </body>
     </html>

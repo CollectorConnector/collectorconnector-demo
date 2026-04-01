dedbeb596da539d3a52e02c4ba443a6af4651243
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-04-01 12:19:27 +0100

    Update page.tsx

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,9 +1,22 @@
 // app/profile/[id]/page.tsx
+"use client";
+
 export default function ProfilePage() {
   return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Profile Test</h1>
-      <p>If you can see this, the page is updating correctly.</p>
+    <div className="p-6 text-white flex flex-col items-center">
+      {/* Avatar */}
+      <img
+        src="/default-avatar.png"
+        alt="Profile"
+        className="w-24 h-24 object-cover border shadow"
+        style={{ borderRadius: "35% / 30%" }}
+      />
+
+      {/* Name */}
+      <h1 className="mt-4 text-2xl font-bold">Your Name</h1>
+
+      {/* Username */}
+      <p className="text-gray-400">@username</p>
     </div>
   );
 }

290b090d92d547e792ebdf0b4038bc57bdffe919
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-04-01 20:48:10 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,15 @@
 import "./globals.css";
-import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Collector Connector",
   description: "Where collectors meet",
 };
 
-type RootLayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-black text-white">
-        <div className="page-body max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-          <main>{children}</main>
-        </div>
+        <main className="app-main">{children}</main>
       </body>
     </html>
   );

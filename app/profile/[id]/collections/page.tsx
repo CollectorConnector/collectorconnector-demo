d86020dc7ab344404bc44c02dbdbe8f9011028ab
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-04-04 14:10:54 +0100

    Update page.tsx

--- app/profile/[id]/collections/page.tsx
+++ app/profile/[id]/collections/page.tsx
@@ -1,1 +1,11 @@
+import CollectionsGrid from "@/components/CollectionsGrid";
+
+export default function ProfileCollectionsPage({ params }: { params: { id: string } }) {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-xl font-semibold px-4 py-4">Collections</h1>
+      <CollectionsGrid userId={params.id} />
+    </div>
+  );
+}
 

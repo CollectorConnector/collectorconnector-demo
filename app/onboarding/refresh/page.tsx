d2812a771978ab515e333e58af30879442d5aa9d
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-04-21 19:05:28 +0100

    Create page.tsx

---
+++ app/onboarding/refresh/page.tsx
@@ -0,0 +1,8 @@
+export default function OnboardingRefresh() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Onboarding Interrupted</h1>
+      <p>Please restart the onboarding process.</p>
+    </div>
+  );
+}

5c21c6b513cd8c87f22266bd2e2e78fa33dd23b4
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-04-21 19:04:18 +0100

    Create page.tsx

---
+++ app/onboarding/complete/page.tsx
@@ -0,0 +1,8 @@
+export default function OnboardingComplete() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Onboarding Complete</h1>
+      <p>Your seller account is now active. You can now list items for sale.</p>
+    </div>
+  );
+}

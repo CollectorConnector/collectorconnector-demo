8741eab555d47b3b7f6433474781b96889fc2dad
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-05-11 19:30:43 +0100

    Add PricePalPage component with iframe

---
+++ app/pricepal/page.tsx
@@ -0,0 +1,12 @@
+export default function PricePalPage() {
+  return (
+    <iframe
+      src="https://your-pricepal-domain.com"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
